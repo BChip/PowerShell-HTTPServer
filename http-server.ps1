6a876775c4683cdca6724720ee82167ad218da2c
BChip <bachippi@svsu.edu>
2016-07-01 15:57:45 -0400

    first upload

---
+++ http-server.ps1
@@ -0,0 +1,41 @@
+﻿$routes = @{
+    
+    "/" = { return Get-Content -Path "Link to HTML file goes here" -Raw }
+}
+
+$url = 'http://localhost:8080/'
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add($url)
+$listener.Start()
+
+Write-Host "Listening at $url..."
+
+while ($listener.IsListening)
+{
+    $context = $listener.GetContext()
+    $requestUrl = $context.Request.Url
+    $response = $context.Response
+
+    Write-Host ''
+    Write-Host "> $requestUrl"
+
+    $localPath = $requestUrl.LocalPath
+    $route = $routes.Get_Item($requestUrl.LocalPath)
+
+    if ($route -eq $null)
+    {
+        $response.StatusCode = 404
+    }
+    else
+    {
+        $content = & $route
+        $buffer = [System.Text.Encoding]::UTF8.GetBytes($content)
+        $response.ContentLength64 = $buffer.Length
+        $response.OutputStream.Write($buffer, 0, $buffer.Length)
+    }
+    
+    $response.Close()
+
+    $responseStatus = $response.StatusCode
+    Write-Host "< $responseStatus"
+}
